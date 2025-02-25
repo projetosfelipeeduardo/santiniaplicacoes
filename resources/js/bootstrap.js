0889e58bfaad62e43b6300be0422926dc3f7ee45
Felipe Eduardo <146198018+projetosfelipeeduardo@users.noreply.github.com>
2025-02-25 15:33:23 -0300

    subindo boostrap

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,11 @@
+window._ = require('lodash');
+
+try {
+    window.Popper = require('@popperjs/core').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
